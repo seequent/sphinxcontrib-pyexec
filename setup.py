3f76d07d2c3b979e62de758c1b93d7258a3f8c05
Brendan Smithyman <brendan@3ptscience.com>
2016-06-14 12:17:50 -0400

    Set up automatic deploy on merge to master.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open('README.md') as fp:
 
 setup(
     name = 'sphinxcontrib-pyexec',
-    version = '0.0.3',
+    version = '0.0.4',
     packages = find_packages(),
     install_requires = ['sphinx',
                         'docutils',
